783ea4d404b995055aed3857d27c4e7bc56894c9
csheffels <christophersheffels@gmail.com>
2016-05-17 10:26:44 -0500

    Made player movement, weapons switching still broken
    Deleted unnecessary standard assets

---
+++ Assets/Swording.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Sword : MonoBehaviour {
+	public bool active = false;
+	// Use this for initialization
+	void Start () 
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if(active == true)
+		{
+			Debug.Log("Switched to sword");
+		}
+	}
+}
