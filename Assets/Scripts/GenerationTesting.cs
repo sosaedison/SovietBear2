3510c14a67a8d32857352201e888be8038d2768f
csheffels <christophersheffels@gmail.com>
2016-05-17 21:53:25 -0500

    Just to get things up to date, you can ignore the random number generation

---
+++ Assets/Scripts/GenerationTesting.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GenerationTesting : MonoBehaviour {
+	// Use this for initialization
+	void Start () 
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () 
+	{
+		int RoomExits = Random.Range(1,11);
+		int ExitPlacement = Random.Range(1,5);
+		if(Input.GetKeyDown(KeyCode.Space))
+		{
+			Debug.Log(""+RoomExits.ToString()+","+ExitPlacement.ToString());
+		}
+	}
+}
