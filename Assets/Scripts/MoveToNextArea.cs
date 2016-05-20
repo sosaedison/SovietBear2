b6018330cb36060674512a027db1fa2ff0793b63
THATBOYJONES <javahnj@gmail.com>
2016-05-20 10:34:46 -0500

    fixing cameras

--- Assets/Scripts/MoveToNextArea.cs
+++ Assets/Scripts/MoveToNextArea.cs
@@ -4,16 +4,18 @@ using System.Collections;
 public class MoveToNextArea : MonoBehaviour
 {
 	public GameObject camera;
-	public GameObject camHere;
+	private GameObject camHere;
 	public BoxCollider2D nextArea;
 	public BoxCollider2D comeBack;
-	public GameObject player;
+	private GameObject player;
 	private BoxCollider2D playerCol;
 	private Vector2 currentPos;
 	private Vector2 endPos;
 	// Use this for initialization
 	void Start ()
 	{
+		camHere = GameObject.FindWithTag ("CamHere"); 
+		player = GameObject.Find ("player"); 
 		endPos = camHere.transform.position;
 		playerCol = GameObject.Find ("player").GetComponent<BoxCollider2D> ();
 		Physics2D.IgnoreCollision (comeBack, playerCol, true);
@@ -28,6 +30,8 @@ public class MoveToNextArea : MonoBehaviour
 	{
 		if (other.gameObject.name == "player") {
 			player.GetComponent<PlayerMovement> ().enabled = false;
+		}
+		if (Vector2.Distance (player.transform.position, camHere.transform.position) <= 53) {
 			camera.transform.position = Vector2.Lerp (endPos, currentPos, 0.1F * Time.deltaTime);
 		}
 
--- Assets/Scripts/Movement Scripts/PlayerMovement.cs
+++ Assets/Scripts/Movement Scripts/PlayerMovement.cs
@@ -1,26 +1,26 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class PlayerMovement : MonoBehaviour {
+public class PlayerMovement : MonoBehaviour
+{
 
 	public int xVelocityFactor = 5;
 	float xMovement = 0.0f;
 	Rigidbody2D rigbod;
 	public bool canJump = true;
-	int jumpPower = 300;
+	int jumpPower = 800;
 	int jetpackPower = 8;
-	float jetpackDuration = 3;
+	float jetpackDuration = 100;
 
 	// Use this for initialization
-	void Start () 
+	void Start ()
 	{
-		rigbod = GetComponent<Rigidbody2D>();
+		rigbod = GetComponent<Rigidbody2D> ();
 	}
 
 	void Update ()
 	{
-		if (canJump == false)
-		{
+		if (canJump == false) {
 			jetpackDuration -= Time.deltaTime;
 		}
 	}
@@ -28,39 +28,33 @@ public class PlayerMovement : MonoBehaviour {
 	// Update is called once per frame
 	void FixedUpdate ()
 	{
-		xMovement = (Input.GetAxis("Horizontal"));
-		rigbod.velocity = new Vector2(xMovement*xVelocityFactor, rigbod.velocity.y);
+		xMovement = (Input.GetAxis ("Horizontal"));
+		rigbod.velocity = new Vector2 (xMovement * xVelocityFactor, rigbod.velocity.y);
 
-		if (canJump == true && Input.GetButtonDown("Vertical") && Input.GetAxisRaw("Vertical")>0)
-		{
-			rigbod.AddForce(new Vector2(0f, jumpPower));
-		}
-		else if(canJump == false && Input.GetButton("Vertical") && Input.GetAxisRaw("Vertical")>0 && jetpackDuration >= 0)
-		{
-			rigbod.AddForce(new Vector2(0f, jetpackPower));
-			Debug.Log("Jetpacking");
+		if (canJump == true && Input.GetButtonDown ("Vertical") && Input.GetAxisRaw ("Vertical") > 0) {
+			rigbod.AddForce (new Vector2 (0f, jumpPower));
+		} else if (canJump == false && Input.GetButton ("Vertical") && Input.GetAxisRaw ("Vertical") > 0 && jetpackDuration >= 0) {
+			rigbod.AddForce (new Vector2 (0f, jetpackPower));
+			Debug.Log ("Jetpacking");
 		}
 	}
 
 	//Enables Jumping
-	void OnTriggerEnter2D(Collider2D other)
+	/*void OnTriggerEnter2D (Collider2D other)
 	{
-		if (other.gameObject.layer == 8)
-		{
+		if (other.gameObject.layer == 8) {
 			canJump = true;
 			rigbod.velocity = new Vector2 (0, 0);
-			if (jetpackDuration <= 3)
-			{
+			if (jetpackDuration <= 3) {
 				jetpackDuration = 3;
 			}
 		}
 	}
 
-	void OnTriggerExit2D(Collider2D other)
+	void OnTriggerExit2D (Collider2D other)
 	{
-		if (other.gameObject.layer == 8)
-		{
+		if (other.gameObject.layer == 8) {
 			canJump = false;
 		}
-	}
+	}*/
 }
