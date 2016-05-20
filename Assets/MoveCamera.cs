1efbd098171a18b80d1dede883e07b86548811a0
THATBOYJONES <javahnj@gmail.com>
2016-05-20 14:15:54 -0500

    Made camera animate and added more triggers

---
+++ Assets/MoveCamera.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveCamera : MonoBehaviour
+{
+	public Vector3 startPos;
+	public Vector3 endPos;
+	public float startTime = 0;
+	public float speed;
+
+	// Use this for initialization
+	void Start ()
+	{
+		startPos = this.transform.position;
+		endPos = this.transform.position;
+	}
+	// Update is called once per frame
+	void Update ()
+	{
+		if (this.transform.position != endPos) {
+			Vector3 currentPos = this.transform.position;
+			endPos.z = currentPos.z;
+			float totalDistance = Vector3.Distance (startPos, endPos);
+			float distanceTravelled = (Time.time - startTime) * speed;
+			this.transform.position = Vector3.Lerp (currentPos, endPos, distanceTravelled / totalDistance);
+		}
+
+	
+	}
+}
--- Assets/Scripts/MoveToNextArea.cs
+++ Assets/Scripts/MoveToNextArea.cs
@@ -4,21 +4,16 @@ using System.Collections;
 public class MoveToNextArea : MonoBehaviour
 {
 	public GameObject camera;
-	private GameObject camHere;
-	public BoxCollider2D nextArea;
-	public BoxCollider2D comeBack;
 	private GameObject player;
 	private BoxCollider2D playerCol;
-	private Vector2 currentPos;
-	private Vector2 endPos;
+	private Vector3 currentPos;
+	public int direction;
 	// Use this for initialization
 	void Start ()
 	{
-		camHere = GameObject.FindWithTag ("CamHere"); 
 		player = GameObject.Find ("player"); 
-		endPos = camHere.transform.position;
+		camera = GameObject.Find ("Main Camera");
 		playerCol = GameObject.Find ("player").GetComponent<BoxCollider2D> ();
-		Physics2D.IgnoreCollision (comeBack, playerCol, true);
 	}
 
 	void FixedUpdate ()
@@ -28,21 +23,12 @@ public class MoveToNextArea : MonoBehaviour
 	// Update is called once per frame
 	void OnTriggerEnter2D (Collider2D other)
 	{
-		if (other.gameObject.name == "player") {
-			player.GetComponent<PlayerMovement> ().enabled = false;
-		}
-		if (Vector2.Distance (player.transform.position, camHere.transform.position) <= 53) {
-			camera.transform.position = Vector2.Lerp (endPos, currentPos, 0.1F * Time.deltaTime);
-		}
-
+		MoveCamera moveCamera = camera.GetComponent<MoveCamera> ();
+		moveCamera.endPos = this.GetComponentInParent<LevelTile> ().adjacentTiles [direction].transform.position;
+		moveCamera.startPos = camera.transform.position;
+		moveCamera.startTime = Time.time;
 			
 	}
 
-	void OnTriggerExit2D (Collider2D other)
-	{
-		player.GetComponent<PlayerMovement> ().enabled = true;
-		Physics2D.IgnoreCollision (nextArea, playerCol, true);
-		Physics2D.IgnoreCollision (comeBack, playerCol, false);
 
-	}
 }
