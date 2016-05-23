79c13a25307eba62afefda36396760b5dbe601a6
csheffels <christophersheffels@gmail.com>
2016-05-23 11:22:06 -0500

    Started work on HUD

---
+++ Assets/Scripts/HUDInteraction.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+public class HUDInteraction : MonoBehaviour {
+
+	Health playerHealth;
+	List<GameObject> HeartPieces;
+	int LastHeartPlaced = 3;
+	public GameObject TLQuarter;
+	public GameObject BLQuarter;
+	public GameObject BRQuarter;
+	public GameObject TRQuarter;
+	// From TL counter clockwise
+
+	// Use this for initialization
+	void Start () 
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		
+	}
+}
