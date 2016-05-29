8b9fb1a5b96b30ec54a65351900c4298088f91ce
Jonathan Solomon <jonathanslmn11@detinu.com>
2016-05-29 17:51:03 -0500

    started on teleporter to final boss

---
+++ Assets/Scripts/FinalBoss/Teleporter.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class Teleporter : MonoBehaviour {
+
+    public Sprite activeSprite;
+
+    bool canTeleport = true;
+
+    public void enableTelporter()
+    {
+        GetComponent<SpriteRenderer>().sprite = activeSprite;
+        canTeleport = true;
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (canTeleport && other.CompareTag("Player"))
+        {
+            canTeleport = false;
+            transform.parent = null;
+            DontDestroyOnLoad(gameObject);
+            DontDestroyOnLoad(other.gameObject);
+            SceneManager.LoadScene("FinalBossBattle");
+            Scene battleScene = SceneManager.GetSceneByName("FinalBossBattle");
+            transform.position = new Vector3(138f, 72.18f);
+            other.transform.position = new Vector3(138f, 72.18f);
+            FindObjectOfType<FinalBossLevelManager>().Pause();
+            Destroy(gameObject);
+        }
+    }
+}
